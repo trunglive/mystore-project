e5f513e00c1f67e6649eadc695f8e452e2e3541c
Trung Vo <trunglive@gmail.com>
2021-11-18 22:40:22 +0700

    feat: add component product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    ConfirmationComponent
+    ConfirmationComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
