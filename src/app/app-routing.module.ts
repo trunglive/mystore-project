bd61d166133a08f3e334d3b0834167ed0d5744f2
Trung Vo <trunglive@gmail.com>
2021-12-27 16:43:26 +0700

    feat: add initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 
-
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'cart', component: CartComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
