5c74762d36fbb55f61f7fa3d46440f246b86f563
Trung Vo <trunglive@gmail.com>
2021-12-27 22:40:38 +0700

    feat: add nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CartComponent } from './cart/cart.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -13,18 +14,15 @@ import { ProductItemDetailComponent } from './product-item-detail/product-item-d
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     CartComponent,
     ConfirmationComponent,
     ProductListComponent,
     ProductItemComponent,
-    ProductItemDetailComponent
+    ProductItemDetailComponent,
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
