abac071591107dab59c87b7d576d9d770e1440a9
Trung Vo <trunglive@gmail.com>
2021-12-28 11:04:24 +0700

    feat: add service Product

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/Product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+    return this.http.get<Product[]>(
+      'https://jsonplaceholder.typicode.com/products?_limit=10'
+    );
+  }
+}
