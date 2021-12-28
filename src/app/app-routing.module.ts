2437b809c1547d84277ef0c797a9cf30a1e0ba43
Trung Vo <trunglive@gmail.com>
2021-12-28 22:11:54 +0700

    feat: add route for product item detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
+  { path: 'product/:productId', component: ProductItemDetailComponent },
   { path: 'cart', component: CartComponent },
 ];
 
