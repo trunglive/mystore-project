9f14f922eeb35ca311bfd23817bb43ec5dd5be03
Trung Vo <trunglive@gmail.com>
2021-12-30 16:44:13 +0700

    fix: show product item detail based on product id param in route

--- src/app/product-item-detail/product-item-detail.component.ts
+++ src/app/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Product } from '../models/Product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
+  productItem: Product = {
+    id: 0,
+    name: '',
+    price: 0,
+    url: '',
+    description: '',
+    quantity: 0,
+  };
+  productId: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private productService: ProductService,
+    private activeRouter: ActivatedRoute
+  ) {
+    this.activeRouter.paramMap.subscribe((params: ParamMap) => {
+      this.productId = Number(params.get('productId'));
+    });
   }
 
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => {
+      this.productItem = (products.find(
+        (product) => product.id === this.productId
+      ) as unknown) as Product;
+    });
+  }
 }
