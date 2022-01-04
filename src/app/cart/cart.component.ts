5ba4e731394d283e01406474b47b374176864404
Trung Vo <trunglive@gmail.com>
2022-01-04 18:51:10 +0700

    fix: show items purchased in cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { Product } from '../models/Product';
 
 @Component({
   selector: 'app-cart',
@@ -7,11 +8,13 @@ import { CartService } from '../services/cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartList: Product[] = [];
   cartTotal: number = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.cartList = this.cartService.getCart();
     this.getCartTotal();
   }
 
