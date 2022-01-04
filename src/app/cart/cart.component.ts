5d85b1daf4abd872ca14719f9b96a14d6a51f86d
Trung Vo <trunglive@gmail.com>
2022-01-04 18:39:07 +0700

    feat: retrieve cart total in cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartTotal: number = 0;
 
-  constructor() { }
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
+    this.getCartTotal();
   }
 
+  getCartTotal() {
+    this.cartTotal = this.cartService.calculateCartTotal();
+  }
 }
